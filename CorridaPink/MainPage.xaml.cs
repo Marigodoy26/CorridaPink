4f03cd7aa7ff9b9b31469e32de115cad1d9f7c42
Mari <mariannasantiagogodoy58@gmail.com>
2024-11-11 11:50:38 -0300

    imagens

--- CorridaPink/MainPage.xaml.cs
+++ CorridaPink/MainPage.xaml.cs
@@ -11,7 +11,7 @@ public partial class MainPage : ContentPage
 	int velocidade1 = 0;
 	int velocidade2 = 0;
 	int velocidade3 = 0;
-	int velocidade =0;
+	int velocidade = 0;
 
 
 	public MainPage()
@@ -37,18 +37,18 @@ public partial class MainPage : ContentPage
 	void CorrigeTamanhoCenario(double w, double h)
 	{
 		foreach (var a in HSLayer1.Children)
-		(a as Image).WidthRequest=w;
+			(a as Image).WidthRequest = w;
 		foreach (var a in HSLayer2.Children)
-		(a as Image).WidthRequest=w;
+			(a as Image).WidthRequest = w;
 		foreach (var a in HSLayer3.Children)
-		(a as Image).WidthRequest=w;
+			(a as Image).WidthRequest = w;
 		foreach (var a in HSLayerChao.Children)
-		(a as Image).WidthRequest=w;
+			(a as Image).WidthRequest = w;
 
-		HSLayer1.WidthRequest=w*1.5;
-		HSLayer2.WidthRequest=w*1.5;
-		HSLayer3.WidthRequest=w*1.5;
-		HSLayerChao.WidthRequest=w*1.5;
+		HSLayer1.WidthRequest = w * 1.5;
+		HSLayer2.WidthRequest = w * 1.5;
+		HSLayer3.WidthRequest = w * 1.5;
+		HSLayerChao.WidthRequest = w * 1.5;
 	}
 
 	void GerenciaCenarios()
@@ -59,79 +59,107 @@ public partial class MainPage : ContentPage
 		GerenciaCenario(HSLayer3);
 		GerenciaCenario(HSLayerChao);
 	}
-    
+
 	void MoveCenario()
 	{
-		HSLayer1.TranslationX-=velocidade1;
-		HSLayer2.TranslationX-=velocidade2;
-		HSLayer3.TranslationX-=velocidade3;
-		HSLayerChao.TranslationX-=velocidade;
+		HSLayer1.TranslationX -= velocidade1;
+		HSLayer2.TranslationX -= velocidade2;
+		HSLayer3.TranslationX -= velocidade3;
+		HSLayerChao.TranslationX -= velocidade;
 	}
 
 	void GerenciaCenario(HorizontalStackLayout HSL)
 	{
-		var view=(HSL.Children.First()as Image);
-		if(view.WidthRequest+HSL.TranslationX<0)
+		var view = (HSL.Children.First() as Image);
+		if (view.WidthRequest + HSL.TranslationX < 0)
 		{
 			HSL.Children.RemoveAt(view);
 			HSL.Children.Add(view);
-			HSL.TranslationX=view.TranslationX;
-		}		
+			HSL.TranslationX = view.TranslationX;
+		}
 	}
 
 	async Task Desenha()
 	{
-		while(!EstaMorto)
+		while (!EstaMorto)
 		{
 			GerenciaCenarios()
 			await Task.Delay(TempoEntreFrames);
 		}
 	}
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
 		Desenha();
-    }
+	}
 
 	public class Animação
 	{
-		protected List<String> animação1= new List<String>();
-		protected List<String> animação2= new List<String>();
-		protected List<String> animação3= new List<String>();
+		protected List<String> animação1 = new List<String>();
+		protected List<String> animação2 = new List<String>();
+		protected List<String> animação3 = new List<String>();
 
-		protected bool loop=true;
-		protected int AnimacaoAtiva=1;
+		protected bool loop = true;
+		protected int AnimacaoAtiva = 1;
 		protected Image compImage;
 		public Animação(Image a)
 		{
-			compImage=a;
+			compImage = a;
 		}
 
 		public void Stop()
 		{
-			parado=true;
+			parado = true;
 		}
 		public void Play()
 		{
-			parado=false;
+			parado = false;
 		}
 		public void SetAnimaçãoAtiva(int a)
 		{
-			SetAnimacaoAtiva=a;
+			SetAnimacaoAtiva = a;
 		}
 	}
 
 	public void Desenha()
 	{
-		if(parado)
-		return;
-		String NomeArquivo="01";
-		int TamanhoAnimacao=0;
-		if(AnimacaoAtiva==1)
+		if (parado)
+			return;
+		String NomeArquivo = "01";
+		int TamanhoAnimacao = 0;
+		if (AnimacaoAtiva == 1)
 		{
-			NomeArquivo=animacao1[Frameatual]
+			NomeArquivo = animacao1[Frameatual];
+			TamanhoAnimacao = animacao1.count;
+		}
+		else if (animacaoAtiva == 2)
+		{
+			NomeArquivo = animacao2[Frameatual];
+			TamanhoAnimacao = animacao2.count;
+		}
+		else if (animacaoAtiva == 3)
+		{
+			NomeArquivo = animacao3[Frameatual];
+			TamanhoAnimacao = animacao3.count;
+		}
+		CompImage.Source = ImageSource.FromFile(NomeArquivo);
+		FrameAtual++;
+		if (FrameAtual >= TamanhoAnimacao)
+		{
+			if (loop)
+				FrameAtual = 0;
+			else
+			{
+				Parado = true;
+				QuandoParar();
+			}
 		}
 	}
+	public virtual void QuandoParar()
+	{
+
+	}
+
 }
 
